3f9257b1ab96d15be1b614857957bf9ccd4879d3
RATHIpriya-777 <rathipriya513@gmail.com>
2026-02-10 13:39:47 +0530

    post_method.py file is added

---
+++ post_method.py
@@ -0,0 +1,21 @@
+import requests
+
+# The URL for the POST method as seen in your image
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+# The data you want to send (must be a dictionary)
+my_data = {
+    'title': 'Hello World',
+    'body': 'This is my first post from VS Code!',
+    'userId': 1
+}
+
+# Sending the POST request
+response = requests.post(url, json=my_data)
+
+# Checking the result
+if response.status_code == 201:
+    print("Success! Created post:")
+    print(response.json())
+else:
+    print(f"Failed with status code: {response.status_code}")
